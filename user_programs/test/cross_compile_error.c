eba8aab581bfab2c56521cb1ad9da6e10e7fed91
Taner Güven <taner@taner-GA-A75-D3H.(none)>
2012-04-27 22:54:33 +0300

    user_programs/bin derlenme hatasi durumunda duzeltme

--- user_programs/test/cross_compile_error.c
+++ user_programs/test/cross_compile_error.c
@@ -8,9 +8,7 @@ int main(int argc, char** argv) {
 
 	printf("HATA!\n"
 		   "user_programs/bin icerisindeki program derlenmemis.\n"
-		   "tools derlenmemis olabilir\n"
-		   "duzeltmek icin:\n"
-		   "  make clean clean-tools all-tools"
+		   "program kodu hatali ya da tools derlenmemis olabilir\n"
 		   "\n\n");
 
 	return 0;
